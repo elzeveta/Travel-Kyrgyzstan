bbccee61b6710b966918edfdf2016082135306de
elzeveta <eliza.amanbaeva@gmail.com>
2021-06-11 03:02:08 -0700

    Added video and components

---
+++ react-travel-website/src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HeroSection() {
+    return (
+        <div className="hero-container">
+            <video src="./videos/video-1.mp4" autoPlay loop muted />
+            <h1>ADVENTURE AWAIT</h1>
+        </div>
+    )
+}
+
+export default HeroSection
