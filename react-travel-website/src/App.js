7e9a15a088492c44a45fe5edad0e470545e91fca
elzeveta <eliza.amanbaeva@gmail.com>
2021-06-11 04:43:40 -0700

    Completed components and routes

--- react-travel-website/src/App.js
+++ react-travel-website/src/App.js
@@ -1,7 +1,12 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AboutKR from './components/pages/About-KR';
+import Tours from './components/pages/Tours';
+import Transport from './components/pages/Transport'
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -9,10 +14,13 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact />
+          <Route path='/' exact component={Home} />
+          <Route path='/about-kr' component={AboutKR} />
+          <Route path='/tours' component={Tours} />
+          <Route path='/transport' component={Transport} />
+          <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
-
     </>
   );
 }
--- react-travel-website/src/components/pages/Tours.js
+++ react-travel-website/src/components/pages/Tours.js
@@ -2,5 +2,6 @@ import React from 'react';
 import '../../App.css';
 
 export default function Tours() {
-  return <h1 className='tours'>TOURS</h1>;
+  return <h1 className='tours'>TOURS</h1>,
+  <a href="http://triptokyrgyzstan.com/en/tours"></a>;
 }
