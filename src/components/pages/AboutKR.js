ab83ecff8f3a727bb718afcc65b3528ffa3cd941
elzeveta <eliza.amanbaeva@gmail.com>
2021-06-11 23:31:53 -0700

    Made changes

---
+++ src/components/pages/AboutKR.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../../App.css';
+
+export default function AboutKR() {
+{/* <div className='about'>
+      <h1>About Kyrgyzstan</h1>
+      <div className='about__container'>
+        <div className='about__wrapper'>
+         <ul className='about__items'>
+            <CardItem
+              src='images/img-3.jpg'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/aboutkr'
+            />
+            <CardItem
+              src='images/img-4.jpg'
+              text='General Information'
+              label='Adventure'
+              path='/aboutkr'
+            />
+            <CardItem
+              src='images/img-8.jpg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/aboutkr'
+            />
+          </ul>
+          <ul className='about__items'>
+            <CardItem
+              src='images/img-9.jpg'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/tours'
+            />
+            <CardItem
+              src='images/img-2.jpg'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/tours'
+            />
+          </ul>
+         
+        </div>
+      </div>
+    </div> */}
+}
