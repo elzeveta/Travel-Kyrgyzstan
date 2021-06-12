3a7c28a6a656b70f2ff22ef541ea067e504550ee
elzeveta <68719361+elzeveta@users.noreply.github.com>
2021-06-11 22:08:56 -0700

    Delete react-travel-website directory

--- react-travel-website/src/components/pages/About-KR.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../../App.css';
-
-export default function AboutKR() {
-  return <h1 className='aboutkr'>About Kyrgyzstan</h1>;
-}
--- react-travel-website/src/components/pages/Tours.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import '../../App.css';
-
-export default function Tours() {
-  return <h1 className='tours'>TOURS</h1>,
-  <a href="http://triptokyrgyzstan.com/en/tours"></a>;
-}
