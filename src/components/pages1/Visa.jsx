5a2eb04c02ca07d8aa750ab6b172eaf3f1f85e1f
elzeveta <eliza.amanbaeva@gmail.com>
2021-06-13 11:06:48 -0700

    Fixed Visa component

---
+++ src/components/pages1/Visa.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./GenInfo.css"
+
+function GenInfo() {
+    return (
+        <div className="gen">
+            <p>Kyrgyzstan – is a country located in Central Asia. Landlocked and mountainous, Kyrgyzstan is bordered by Kazakhstan to the north, Uzbekistan to the west, Tajikistan to the southwest and China to the east. Its capital and largest city is Bishkek.</p>
+            <p>The official language, Kyrgyz, is closely related to the other Turkic languages; however, the country is under a strong cultural influence from Russia and is rather Russified. The majority of the population (64%) are nondenominational Muslims.</p>
+            <p>Kyrgyzstan is a member of the Commonwealth of Independent States, the Eurasian Economic Community, the Collective Security Treaty Organization, the Shanghai Cooperation Organisation, the Organisation of Islamic Cooperation, the Turkic Council, the TÜRKSOY community and the United Nations.</p>
+        </div>
+    );
+}
+
+export default GenInfo;
