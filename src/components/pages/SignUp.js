2b6e05201b74523fc1304b2d58afa0e2dc25e6f5
elzeveta <eliza.amanbaeva@gmail.com>
2021-06-19 00:06:41 -0700

    Last Changes

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,5 +2,9 @@ import React from 'react';
 import '../../App.css';
 
 export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+  return (<div className='hero-container'>
+  <video src='/videos/video-1.mp4' autoPlay loop muted />
+  <h1>Thank You For Visiting!</h1>
+  </div>
+  )
 }
